ed774732254409a116868bc9178380826c1272b0
oshico <oshico.dev@gmail.com>
2026-05-16 14:31:50 +0000

    fix: changed go docker files path

--- frontend/src/routes/about/$locationId.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/about/$locationId')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/about/$locationId"!</div>
-}
--- frontend/src/routes/view.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/view')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/view"!</div>
-}
